62098a8349632852818cb59d2b2b7abe70fa1a9f
Arpan Saha <arpansaha230227@gmail.com>
2026-04-12 22:53:21 +0530

    CORS configured

--- spvs-backend/server.js
+++ spvs-backend/server.js
@@ -9,7 +9,14 @@ dotenv.config()
 connectDB()
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://spvs.vercel.app'
+  ],
+  credentials: true
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
