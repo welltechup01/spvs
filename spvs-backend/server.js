4704c355551b4d910cdda9295a724a3c5a7cc150
Arpan Saha <arpansaha230227@gmail.com>
2026-04-19 10:24:17 +0530

    Fix CORS for Vercel frontend

--- spvs-backend/server.js
+++ spvs-backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://spvs.vercel.app'
+    'https://spvs-rho.vercel.app'
   ],
   credentials: true
 }))
